f2f222389bb5d2d4dca4cf26f94e96ea8fef6508
Anton Pavl <antonpavl96@gmail.com>
2022-03-14 19:02:04 +0200

    added lyrics finder function

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const lyricsFinder = require("lyrics-finder");
 
 const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
@@ -53,6 +55,13 @@ app.post("/refresh", (req, res) => {
       console.log(err);
       res.sendStatus(400);
     });
+});
+
+app.get("/lyrics", async (req, res) => {
+  const lyrics =
+    (await lyricsFinder(req.query.artist, req.query.track)) ||
+    "No Lyrics Found";
+  res.json({ lyrics });
 });
 
 app.listen(1000);
